1c979b7c8818982c7ba4df4de7b095e55571c056
Adrian Käsper <adriank@tlu.ee>
2023-09-13 15:38:54 +0300

    13.06.23 Tunnitöö

--- Tunnitöö/13.09.23/basics.js
+++ Tunnitöö/13.09.23/basics.js
@@ -1,8 +1,8 @@
 let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(findMyIndex(numberArray, 3));
-console.log(addNumbers2(2)(4));
-console.log(newArray)
-
+//console.log(addNumbers2(2)(4));
+//console.log(newArray)
+//console.log({numberArray, filterArray})
 
 function findMyIndex(array, element){
     return array.indexOf(element);
@@ -26,4 +26,39 @@ const nameString = (name) => `hello ${name}`
 
 const newArray = numberArray.map((element, index, array) => {return element + 5})
 
-const filterArray = numberArray.filter()
+const filterArray = numberArray.filter(num => num < 4)
+
+const names = ['Juku', 'Mari', 'Jüri', 'Jaana']
+
+/*const dataa = name.map(name => {
+    return {
+        name: "name1",
+        age: 123,
+        email: "afafaf@company.com",
+        address: 
+    }
+})*/
+
+const data = names.map(names => {
+    return {
+        name: names,
+        age: names.length + 20,
+        email: names.toLowerCase() + "@company.com",
+        address: `${names} Street`,
+        username: names.split("").reverse().join(""),
+
+    }
+})
+
+const newJuku = {
+    ...data[0],
+    height: 175
+}
+
+const evenNewerJuku = {
+    ...newJuku,
+    age: 99
+}
+console.log(data)
+console.log(newJuku)
+console.log(evenNewerJuku)
